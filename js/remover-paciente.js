75e8bd08a30f1686b9bbeb1c0155d73a04302e59
laiana-25 <laiana.ggcs@gmail.com>
2020-08-28 11:51:21 -0300

    Minhas mudanças efetuadas

---
+++ js/remover-paciente.js
@@ -0,0 +1,25 @@
+//var pacientes = document.querySelectorAll(".paciente");
+
+var tabela = document.querySelector("table");
+
+tabela.addEventListener("dblclick", function(event){
+    event.target.parentNode.classList.add("fadeOut"); //remove a linha mais devagar, foi criado class no 
+    
+    setTimeout(function(){
+        event.target.parentNode.remove();
+    },500);
+});
+
+// pacientes.forEach(function(paciente){
+//     paciente.addEventListener("dblclick", function(){
+//         console.log("Fui clicado com um duplo click");
+//         this.remove(); //this é o dono do evento, tomar cuidado quando usar ele
+//     });
+//}); // remove paciente com 2 clicks
+
+    //var alvoEvento = event.target;
+    //var paiDoAlvo = alvoEvento.parentNode;
+    //paiDoAlvo.remove(); ou tudo em uma linha como abaixo
+
+    //event.target.parentNode.remove(); //remove instantaneamente a linha
+
